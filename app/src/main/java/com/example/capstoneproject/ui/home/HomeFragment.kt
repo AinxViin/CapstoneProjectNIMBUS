59517f2f91a3b5fbf202bbe5b642180ca109c7d9
AinxViin <kevinagustin2208@gmail.com>
2024-12-04 09:29:31 +0700

    Initial commit

--- app/src/main/java/com/example/capstoneproject/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/capstoneproject/ui/home/HomeFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.R
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.capstoneproject.adapter.ProvinceAdapter
